3d9d22d5ad7a618293e5edbee84aa93d862b61d4
Jay27 <jannes.jy12321@gmail.com>
2026-05-31 11:28:09 +0200

    database

--- database.js
+++ database.js
@@ -5,17 +5,17 @@
 
 // --- 1. BASE ITEMS & EQUIPMENT ---
 const itemDatabase = {
-    key:      { name:'Key',      desc:'Opens mysterious doors',      img:'../assets/key.png' },
-    compass:  { name:'Compass',  desc:'Shows where you are on the map',  img:'../assets/compass.png', slot: 'offHand' },
-    map:      { name:'Map',      desc:'Unlocks the minimap',         img:'../assets/map-item.png' },
-    backpack: { name:'Backpack', desc:'Allows you to carry items',   img:'../assets/backpack.png' },
-    pickaxe:  { name:'Pickaxe',  desc:'Used to mine ores',           img:'../assets/pickaxe.png', slot: 'mainHand' },
-    satchel:  { name:'Satchel',  desc:'Holds heavy ores and ingots', img:'../assets/satchel.png' }
+    key:      { name: 'Key',      desc: 'Opens mysterious doors',      img: '../assets/key.png' },
+    compass:  { name: 'Compass',  desc: 'Shows where you are on the map', img: '../assets/compass.png', slot: 'offHand' },
+    map:      { name: 'Map',      desc: 'Unlocks the minimap',         img: '../assets/map-item.png' },
+    backpack: { name: 'Backpack', desc: 'Allows you to carry items',   img: '../assets/backpack.png' },
+    pickaxe:  { name: 'Pickaxe',  desc: 'Used to mine ores',           img: '../assets/pickaxe.png', slot: 'mainHand' },
+    satchel:  { name: 'Satchel',  desc: 'Holds heavy ores and ingots', img: '../assets/satchel.png' }
 };
 
 // --- 2. DEFAULT STARTING MEMORY ---
 const DEFAULT_SATCHEL = {
-    ores: { iron: 0, copper: 0, tin: 0 },
+    ores:   { iron: 0, copper: 0, tin: 0 },
     ingots: { iron: [], copper: [], tin: [] }
 };
 
@@ -27,68 +27,23 @@ const DEFAULT_EQUIPMENT = {
 
 // --- 3. BLACKSMITH RECIPES ---
 const BLACKSMITH_RECIPES = {                                     
-    'iron_sword': {
-        id: 'iron_sword',                                        
-        name: 'Iron Sword',                                      
-        slot: 'mainHand',                                        
-        cost: { type: 'iron', amount: 3 }                        
-    },
-    'copper_helmet': {
-        id: 'copper_helmet',                
-        name: 'Copper Helmet',
-        slot: 'head',
-        cost: { type: 'copper', amount: 2 } 
-    },
-    'ring_of_smithing_forgiveness': {
-        id: 'ring_of_smithing_forgiveness', 
-        name: 'Ring of Forgiveness',
-        slot: 'ring',
-        cost: { type: 'tin', amount: 3 } 
-    },
-    'boots_of_the_traveller': {
-        id: 'boots_of_the_traveller', 
-        name: 'Boots of the Traveller',
-        slot: 'boots',
-        cost: { type: 'copper', amount: 1 }             
-    },
-    'true_compass': {
-        id: 'true_compass', 
-        name: 'True Compass',
-        slot: 'offHand',
-        cost: { type: 'tin', amount: 1 }                
-    },
-    'iron_compass': {
-        id: 'iron_compass', 
-        name: 'Iron Compass',
-        slot: 'offHand',
-        cost: { type: 'iron', amount: 1 }                
-    }
+    iron_sword:                  { id: 'iron_sword',                  name: 'Iron Sword',             slot: 'mainHand', cost: { type: 'iron',   amount: 3 } },
+    copper_helmet:               { id: 'copper_helmet',               name: 'Copper Helmet',          slot: 'head',     cost: { type: 'copper', amount: 2 } },
+    ring_of_smithing_forgiveness:{ id: 'ring_of_smithing_forgiveness', name: 'Ring of Forgiveness',   slot: 'ring',     cost: { type: 'tin',    amount: 3 } },
+    boots_of_the_traveller:      { id: 'boots_of_the_traveller',      name: 'Boots of the Traveller', slot: 'boots',    cost: { type: 'copper', amount: 1 } },
+    true_compass:                { id: 'true_compass',                name: 'True Compass',           slot: 'offHand',  cost: { type: 'tin',    amount: 1 } },
+    iron_compass:                { id: 'iron_compass',                name: 'Iron Compass',           slot: 'offHand',  cost: { type: 'iron',   amount: 1 } }
 };
 
 // --- 4. MINING CONFIGURATIONS ---
 const MINING_CONFIG = {                          
-    'iron': {
-        requiredTool: 'pickaxe',                 
-        spawnRange: [3, 5],                      
-        baseParticles: 3,                        
-        dropRange: [1, 3]                        
-    },
-    'copper': {
-        requiredTool: 'pickaxe',
-        spawnRange: [4, 6],
-        baseParticles: 2,        
-        dropRange: [1, 4]
-    },
-    'tin': {
-        requiredTool: 'pickaxe',
-        spawnRange: [1, 2],
-        baseParticles: 4,
-        dropRange: [1, 2]
-    }
+    iron:   { requiredTool: 'pickaxe', spawnRange: [3, 5], baseParticles: 3, dropRange: [1, 3] },
+    copper: { requiredTool: 'pickaxe', spawnRange: [4, 6], baseParticles: 2, dropRange: [1, 4] },
+    tin:    { requiredTool: 'pickaxe', spawnRange: [1, 2], baseParticles: 4, dropRange: [1, 2] }
 };
 
 const ITEM_MODIFIERS = {                         
-    'pickaxe': { power: 0, yield: 0 } 
+    pickaxe: { power: 0, yield: 0 } 
 };
 
 // --- 5. DAILY TIPS ---
